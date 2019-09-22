730b334e13d6d0b5f2679120c88afee6da9bad6d
Thai Nguyen <discoverylight@yahoo.com>
2019-09-22 16:26:23 -0700

    added CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const authRoutes = require("./routes/auth");
 const swaggerDoc = require("./swaggerDoc");
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
 
